b6a7911ade1af5d9707a08ec88dbc453c093086d
Branson Camp <camp.branson@gmail.com>
2019-10-16 10:00:29 -0600

    Complete rewrite

--- build/sketch/main.ino.cpp
+++ build/sketch/main.ino.cpp
@@ -1,16 +1,6 @@
 #include <Arduino.h>
 #line 1 "/Users/bransoncamp/Arduino/table-hub/main.ino"
 #line 1 "/Users/bransoncamp/Arduino/table-hub/main.ino"
-/*
-table-hub
-Created by Branson Camp for CECFC on 2019-10-1
-Based on Fischer Moseley's Table-Remote (http://github.com/FischerMoseley/Table-Remote/)
-
------ Master Remote (#1) Codes -----
-UP CODE: 6532833
-DOWN CODE: 6532834
-*/
-
 #include <RCSwitch.h>
 
 // Switches
@@ -18,98 +8,93 @@ RCSwitch receiveSwitch = RCSwitch();
 RCSwitch transmitSwitch = RCSwitch();
 
 // Pins
-const int RECEIVER_INTERRUPT = 0; // This is pin #2 on the Arduino Uno
+const int RECEIVER_INTERRUPT = 0; 
 const int TRANSMITTER_PIN = 10;
-const int BIT_LENGTH = 24; //how many bits to send (used for binary zero padding)
-const int LED_PIN = 13;
+const int BIT_LENGTH = 24; 
+const int LED_PIN = 13; // Onboard LED pin
 
 // Key Codes
 const int MASTER_UP_CODE = 6532833;
 const int MASTER_DOWN_CODE = 6532834;
 
-const long WAIT_TIME = 500; // How long to wait in between sending codes in milliseconds
-const long REPEAT_WAIT_TIME = 15000; // How long to wait in between repeated raising/lowering commands
+const long CODE_WAIT_TIME = 500;
 
-const int TRANSMIT_ITERATIONS = 3; // How many times to repeat transmit of up/down codes
+const int codeNum = 12;
 
-// Down button code = Up button code + 1
-unsigned long int codes[]={ //stores the transmitter up button codes
-  6532833, 
-  13806609,
-  10427409,
-  15956497,
-  78353,
-  13108961,
-  473617,
-  6525457,
-  252945,
-  9861857,
-  14031377,
-  711697,
+typedef unsigned long int Code;
+
+// Up codes for each desk (1-12)
+// Down code = Up code + 1
+Code codes[] = {   
+	6532833, 
+  	13806609,
+  	10427409,
+  	15956497,
+  	78353,
+  	13108961,
+  	473617,
+  	6525457,
+  	252945,
+  	9861857,
+  	14031377,
+  	711697,
 };
 
-int numCodes = 0; // Number of codes in the code array. Evaluated at setup();
+// Prototypes
+void sendCode(Code code);
+void raiseAll();
+void lowerAll();
 
-// State
-enum State {
-    LISTENING,
-    TRANSMIT_UP,
-    TRANSMIT_DOWN,
-} state = State::LISTENING;
-
-#line 57 "/Users/bransoncamp/Arduino/table-hub/main.ino"
-void sendCode(unsigned long int code);
-#line 61 "/Users/bransoncamp/Arduino/table-hub/main.ino"
+#line 45 "/Users/bransoncamp/Arduino/table-hub/main.ino"
 void setup();
-#line 71 "/Users/bransoncamp/Arduino/table-hub/main.ino"
+#line 54 "/Users/bransoncamp/Arduino/table-hub/main.ino"
 void loop();
-#line 57 "/Users/bransoncamp/Arduino/table-hub/main.ino"
-void sendCode(unsigned long int code) {
-    transmitSwitch.send(code, BIT_LENGTH);
-}
-
+#line 45 "/Users/bransoncamp/Arduino/table-hub/main.ino"
 void setup() {
-    Serial.begin(9600);
-    receiveSwitch.enableReceive(RECEIVER_INTERRUPT); // Set up reciever 
-    transmitSwitch.enableTransmit(TRANSMITTER_PIN); // Set up transmitter
-    numCodes = sizeof(codes) / sizeof(codes[0]); // Get number of codes
+	Serial.begin(9600);
+    receiveSwitch.enableReceive(RECEIVER_INTERRUPT);
+    transmitSwitch.enableTransmit(TRANSMITTER_PIN);
     pinMode(LED_PIN, OUTPUT); // Set up on-board LED pin
 	Serial.println("Setup complete");
 	Serial.println("Listening...");
 }
 
 void loop() {
-    switch(state) {
-        case State::LISTENING:
-            if (receiveSwitch.available()) {
-                int value = receiveSwitch.getReceivedValue();
-                if (value == MASTER_UP_CODE) {
-                    state = State::TRANSMIT_UP;
-					Serial.println("Transmitting up codes");
-                } else if (value == MASTER_DOWN_CODE) {
-                    state = State::TRANSMIT_DOWN;
-					Serial.println("Transmitting down codes");
-                }
-                receiveSwitch.resetAvailable();
-            }
-        break;
-        case State::TRANSMIT_UP:
-        case State::TRANSMIT_DOWN:
-            for (int j = 0; j < TRANSMIT_ITERATIONS; j++) {
-                for (int i = 0; i < numCodes; i++) {
-					// Send the code
-					unsigned long int code = codes[i] + 1;
-					transmitSwitch.send(code, BIT_LENGTH);
-					Serial.print("[code] ");
-					Serial.println(code);		
-                    delay(WAIT_TIME);
-                }
-                delay(REPEAT_WAIT_TIME);
-            }
-            state = State::LISTENING;
-			Serial.println("Listening...");
-
-        break;
-    }
+	if (receiveSwitch.available()) {
+		Code value = receiveSwitch.getReceivedValue();
+		if (value == MASTER_UP_CODE) {
+			raiseAll();
+		} else if (value == MASTER_DOWN_CODE) {
+			lowerAll();
+		}	
+		receiveSwitch.resetAvailable();
+	}
 }
+
+void sendCode(Code code) {
+	Serial.print("sending");
+	Serial.println(code);
+	transmitSwitch.send(code, BIT_LENGTH);
+}
+
+void raiseAll() {
+	Serial.println("Transmitting up codes");
+	for (int i = 0; i < codeNum; i++) {
+		Code code = codes[i];	
+		sendCode(code);
+		delay(CODE_WAIT_TIME);
+	}	
+	Serial.println("Done. \nListening...");
+}
+
+void lowerAll() {
+	Serial.println("Transmitting down codes");
+	for (int i = 0; i < codeNum; i++) {
+		Code code = codes[i] + 1;	
+		sendCode(code);
+		delay(CODE_WAIT_TIME);
+	}	
+	Serial.println("Done. \nListening...");
+}
+
 
--- main.ino
+++ main.ino
@@ -1,13 +1,3 @@
-/*
-table-hub
-Created by Branson Camp for CECFC on 2019-10-1
-Based on Fischer Moseley's Table-Remote (http://github.com/FischerMoseley/Table-Remote/)
-
------ Master Remote (#1) Codes -----
-UP CODE: 6532833
-DOWN CODE: 6532834
-*/
-
 #include <RCSwitch.h>
 
 // Switches
@@ -15,100 +5,87 @@ RCSwitch receiveSwitch = RCSwitch();
 RCSwitch transmitSwitch = RCSwitch();
 
 // Pins
-const int RECEIVER_INTERRUPT = 0; // This is pin #2 on the Arduino Uno
+const int RECEIVER_INTERRUPT = 0; 
 const int TRANSMITTER_PIN = 10;
-const int BIT_LENGTH = 24; //how many bits to send (used for binary zero padding)
-const int LED_PIN = 13;
+const int BIT_LENGTH = 24; 
+const int LED_PIN = 13; // Onboard LED pin
 
 // Key Codes
 const int MASTER_UP_CODE = 6532833;
 const int MASTER_DOWN_CODE = 6532834;
 
-const long WAIT_TIME = 500; // How long to wait in between sending codes in milliseconds
-const long REPEAT_WAIT_TIME = 15000; // How long to wait in between repeated raising/lowering commands
+const long CODE_WAIT_TIME = 500;
 
-const int TRANSMIT_ITERATIONS = 3; // How many times to repeat transmit of up/down codes
+const int codeNum = 12;
 
-// Down button code = Up button code + 1
-unsigned long int codes[]={ //stores the transmitter up button codes
-  6532833, 
-  13806609,
-  10427409,
-  15956497,
-  78353,
-  13108961,
-  473617,
-  6525457,
-  252945,
-  9861857,
-  14031377,
-  711697,
+typedef unsigned long int Code;
+
+// Up codes for each desk (1-12)
+// Down code = Up code + 1
+Code codes[] = {   
+	6532833, 
+  	13806609,
+  	10427409,
+  	15956497,
+  	78353,
+  	13108961,
+  	473617,
+  	6525457,
+  	252945,
+  	9861857,
+  	14031377,
+  	711697,
 };
 
-int numCodes = 0; // Number of codes in the code array. Evaluated at setup();
-
-// State
-enum State {
-    LISTENING,
-    TRANSMIT_UP,
-    TRANSMIT_DOWN,
-} state = State::LISTENING;
+// Prototypes
+void sendCode(Code code);
+void raiseAll();
+void lowerAll();
 
 void setup() {
-    Serial.begin(9600);
-    receiveSwitch.enableReceive(RECEIVER_INTERRUPT); // Set up reciever 
-    transmitSwitch.enableTransmit(TRANSMITTER_PIN); // Set up transmitter
-    numCodes = sizeof(codes) / sizeof(codes[0]); // Get number of codes
+	Serial.begin(9600);
+    receiveSwitch.enableReceive(RECEIVER_INTERRUPT);
+    transmitSwitch.enableTransmit(TRANSMITTER_PIN);
     pinMode(LED_PIN, OUTPUT); // Set up on-board LED pin
 	Serial.println("Setup complete");
 	Serial.println("Listening...");
 }
 
 void loop() {
-    switch(state) {
-        case State::LISTENING:
-            if (receiveSwitch.available()) {
-                int value = receiveSwitch.getReceivedValue();
-                if (value == MASTER_UP_CODE) {
-                    state = State::TRANSMIT_UP;
-					Serial.println("Transmitting up codes");
-                } else if (value == MASTER_DOWN_CODE) {
-                    state = State::TRANSMIT_DOWN;
-					Serial.println("Transmitting down codes");
-                }
-                receiveSwitch.resetAvailable();
-            }
-        break;
-        case State::TRANSMIT_UP:
-        case State::TRANSMIT_DOWN:
-			int codeFlag;
-			if (state == TRANSMIT_UP) {
-				codeFlag = 0;
-				Serial.println("---- Transmitting up codes ----");		
-			} else {
-				codeFlag = 1;
-				Serial.println("---- Transmitting down codes ----");		
-			}
-
-            for (int j = 0; j < TRANSMIT_ITERATIONS; j++) {
-				Serial.print("--- Iteration ");
-				Serial.print(j+1);
-				Serial.print("/");
-				Serial.print(TRANSMIT_ITERATIONS);
-				Serial.println(" ---");
-                for (int i = 0; i < numCodes; i++) {
-					unsigned long int code = codes[i] + codeFlag;
-					transmitSwitch.send(code, BIT_LENGTH);
-					Serial.print("[code] ");
-					Serial.println(code);		
-                    delay(WAIT_TIME);
-                }
-				Serial.println("Waiting...")
-                delay(REPEAT_WAIT_TIME);
-            }
-            state = State::LISTENING;
-			Serial.println("Listening...");
-
-        break;
-    }
+	if (receiveSwitch.available()) {
+		Code value = receiveSwitch.getReceivedValue();
+		if (value == MASTER_UP_CODE) {
+			raiseAll();
+		} else if (value == MASTER_DOWN_CODE) {
+			lowerAll();
+		}	
+		receiveSwitch.resetAvailable();
+	}
 }
+
+void sendCode(Code code) {
+	Serial.print("sending");
+	Serial.println(code);
+	transmitSwitch.send(code, BIT_LENGTH);
+}
+
+void raiseAll() {
+	Serial.println("Transmitting up codes");
+	for (int i = 0; i < codeNum; i++) {
+		Code code = codes[i];	
+		sendCode(code);
+		delay(CODE_WAIT_TIME);
+	}	
+	Serial.println("Done. \nListening...");
+}
+
+void lowerAll() {
+	Serial.println("Transmitting down codes");
+	for (int i = 0; i < codeNum; i++) {
+		Code code = codes[i] + 1;	
+		sendCode(code);
+		delay(CODE_WAIT_TIME);
+	}	
+	Serial.println("Done. \nListening...");
+}
+
